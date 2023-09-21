ac4a15fa8b88982ce98a3fbd32e5875df6255844
Vitaly Lavrov <vitaly.lavrov@coralogix.com>
2023-09-21 09:48:12 +0200

    Docs for SHA-1 from libcrypto

--- sha1/digest-native.scala
+++ sha1/digest-native.scala
@@ -6,11 +6,42 @@ import scodec.bits.ByteVector
 import scala.scalanative.unsafe.*
 import scala.scalanative.unsigned.*
 
+/**
+ * https://manpages.ubuntu.com/manpages/trusty/man3/SHA1_Init.3ssl.html
+ *
+ * #include <openssl/sha.h>
+ *
+ * unsigned char *SHA1(const unsigned char *d, unsigned long n,
+ * unsigned char *md);
+ *
+ * int SHA1_Init(SHA_CTX *c);
+ * int SHA1_Update(SHA_CTX *c, const void *data, unsigned long len);
+ * int SHA1_Final(unsigned char *md, SHA_CTX *c);
+ *
+ * SHA-1 (Secure Hash Algorithm) is a cryptographic hash function with a 160 bit output.
+ *
+ * SHA1() computes the SHA-1 message digest of the n bytes at d and places it in md (which
+ * must have space for SHA_DIGEST_LENGTH == 20 bytes of output). If md is NULL, the digest is
+ * placed in a static array.
+ *
+ * The following functions may be used if the message is not completely stored in memory:
+ *
+ * SHA1_Init() initializes a SHA_CTX structure.
+ *
+ * SHA1_Update() can be called repeatedly with chunks of the message to be hashed (len bytes
+ * at data).
+ *
+ * SHA1_Final() places the message digest in md, which must have space for SHA_DIGEST_LENGTH == 20 bytes
+ * of output, and erases the SHA_CTX.
+ * */
+
 @link("crypto")
 @extern
 private object crypto {
   def SHA1_Init(c: Ptr[Byte]): CInt = extern
+
   def SHA1_Update(c: Ptr[Byte], data: Ptr[Byte], len: CSize): CInt = extern
+
   def SHA1_Final(md: CString, c: Ptr[Byte]): CInt = extern
 }
 
